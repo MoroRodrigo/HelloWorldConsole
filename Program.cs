1e7c2e065903bd977f018d6bd594a2427d226e50
Rodrigo Moro <rodrigo.moro.francisco@outlook.com>
2024-03-30 02:43:23 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorldConsole
+{
+    using System;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+        }
+    }
+}
